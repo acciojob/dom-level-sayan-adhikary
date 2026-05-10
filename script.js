bb7e108ee061db6ea316b049251f09f6294d0775
Sayan Adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-05-10 09:37:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+/*  your css code here. If applicable */
+let ele = document.getElementById('level');
+let cnt = 0;
+while(ele){
+	ele = ele.parentElement;
+	cnt++;
+}
+alert(`The level of the element is: ${cnt}`);
